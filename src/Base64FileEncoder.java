1f673edea97a05f0722bd1513ad35ac99c0cad7c
wackozacko <isaac.doub@gmail.com>
2011-04-30 01:48:03 +0100

    wackozacko isaac.doub@gmail.com 30/04/2011
    Created Base64FileEncoder.java. This class takes a
    file or a directory and will convert it to base 64
    and output it to a directory 'base64'.
    
    Command line parameters are as follows:
    
    -h  insert encodings into html (data/png;base64 et
    c). Will hopefully call a future function which wi
    ll actually replace the url where the image was fo
    und with the base64 encoding.
    
    -s  write all encodings to a single file instead o
    f individual files. Titles each chunk with the ori
    ginal filename.
    
    -lt <filesize>  this option sets a maximum to the
    filesize that will be encoded in KB. If an image i
    s larger than this size it is simply skipped.
    
    -c  this only encodes files if the new filesize is
    less than the original filesize * 1.35 . Unfortuna
    tely because base64 is unoptimized the new filesiz
    es are always greater than the original filesize *
    1.35 SO DON'T USE THIS. I have left it in as I am
    unsure whether GZipping the text files will improv
    e this as of yet. If that is the case, I can chang
    e the factor from 1.35 to perhaps greater and then
    it will be a useful feature.

---
+++ src/Base64FileEncoder.java
@@ -0,0 +1,162 @@
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import org.apache.commons.codec.binary.Base64;
+
+public class Base64FileEncoder {
+	
+	private boolean singleFile = false;
+	private boolean insertIntoHTML = false;
+	private boolean fileCompare = false;
+	private boolean lessThan = false;
+	private long lessThanSize = 0;
+	public String eol = System.getProperty("line.separator");
+	public String fs = System.getProperty("file.separator");
+
+	public static void main (String args[]) {
+		Base64FileEncoder program = new Base64FileEncoder();
+		try {
+			program.run(args);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+//*******************************MAIN RUN FUNCTION***************************************//
+	private void run(String args[]) throws IOException{
+		if (args.length < 1) {
+			printHelp();
+			System.exit(9);
+		}
+	if (args.length > 1) {
+		for(int i=1;i<args.length;i++) {
+			String option = args[i];
+			if(option.equals("-h"))
+				insertIntoHTML = true;
+			else if(option.equals("-s"))
+				singleFile = true;
+			else if(option.equals("-c"))
+				fileCompare = true;
+			else if(option.equals("-lt")) {
+				lessThan = true;
+				lessThanSize = Long.parseLong(args[i+1]) * 1024;
+				i++;
+			}
+			else {
+				System.out.println("ERROR:Unrecognized option." + eol);
+				printHelp();
+				System.exit(9);
+			}
+		}
+	}
+	String base64file = null;
+	File[] file = new File(args[0]).listFiles();
+	base64file = getFiles(file);
+	
+	if(singleFile)
+		writeBase64ToFile(base64file, "Base 64 Images");
+	}
+
+//****************************OPEN EACH FILE INDIVIDUALLY********************************//
+	private String getFiles(File[] files) {
+		String base64string = "";
+		for (File file : files) {
+	        if (file.isDirectory()) {
+	            base64string += getFiles(file.listFiles());
+	        } else {
+	        	String tmpBase64 = encodeFile(file.toString());
+	            if(!lessThan && !insertIntoHTML && !singleFile) {
+	            	base64string = tmpBase64;
+		            writeBase64ToFile(base64string, file.getName());
+	            }
+
+	        	if(fileCompare) {
+        			File outFile = new File("base64" + fs + file.getName() + ".txt");
+	        		if(outFile.length() > file.length()*1.35) {
+	        			outFile.delete();
+	        			continue;
+	        		}
+	        	}
+	            
+	        	if(lessThan && file.length() > lessThanSize)
+	        		continue;
+	        	
+	            if(insertIntoHTML) {
+	            	tmpBase64 = "data:image/png;base64," + tmpBase64;
+	            	//insertHTMLstring(base64string,file.toString());
+	            }
+	            if(singleFile) {
+		            base64string += file.getPath() + eol + "**********" + eol + tmpBase64 + eol + eol;
+	            }
+	        }
+	    }
+		if(singleFile)
+			return base64string;
+		else
+			return null;
+	}
+
+//************************************ENCODE EACH FILE***********************************//
+	private String encodeFile(String infile) {
+		FileInputStream fis = null;
+		try {
+			fis = new FileInputStream(infile);
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+		}
+		
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        byte[] buf = new byte[1024];
+        try {
+            for (int readNum; (readNum = fis.read(buf)) != -1;) {
+                bos.write(buf, 0, readNum);
+            }
+        } catch (IOException e2) {
+        	e2.printStackTrace();
+        }
+        byte[] bytes = bos.toByteArray();
+        
+        byte[] base = Base64.encodeBase64(bytes, true);
+        String base64string = new String(base);
+        return base64string;
+	}
+
+//*****************************WRITE BASE 64 TO FILE*************************************//
+	private void writeBase64ToFile(String base64string, String filename) {
+		File dir = new File("base64");
+		if(!dir.exists())
+			dir.mkdir();
+		
+        FileWriter outFileWriter = null;
+		try {
+			outFileWriter = new FileWriter("base64" + fs + filename + ".txt");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+        PrintWriter out = new PrintWriter(outFileWriter);
+        out.write(base64string);
+		
+		out.close();
+	}
+
+//************************************PRINT HELP*****************************************//
+	private void printHelp() {
+	      System.out.println ("Command line parameters: input [options]");
+	      System.out.println ("Options are:");
+	      System.out.println ("\t-h\t\tinsert images found into html");
+	      System.out.println ("\t-s\t\twrite all base 64 conversions to a single file");
+	      System.out.println ("\t-lt <filesize>\tset a maximum filesize to convert in KB");
+	}
+}
+
+//every 5KB sent requires about 1.75KB Overhead (about 35% overhead)
+//therefore, to calculate whether it is worth encoding
+//OF = Original Filesize
+//NF = New Filesize
+//
+// OF*1.35 < NF
